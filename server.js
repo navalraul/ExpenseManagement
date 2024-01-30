f107d6a34687a4c1b5708d549acc23ddc4514edc
navalraul <129669942+navalraul@users.noreply.github.com>
2024-01-30 13:35:51 +0530

    ud

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDb = require('./config/connectDb')
+dotenv.config();
+
+
+connectDb();
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/',(req, res) => {
+    res.send("<h1>Hello</h1>")
+})
+
+const PORT = 8000 || process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
